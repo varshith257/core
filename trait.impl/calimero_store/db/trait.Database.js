dc952f7ffeaac930aa7321dead6e278d51dd411f
varshith257 <varshith257@users.noreply.github.com>
2025-05-29 13:27:39 +0000

    deploy: 4cfdbea2fc005720df59c4630c2ce53df2b31d86

--- trait.impl/calimero_store/db/trait.Database.js
+++ trait.impl/calimero_store/db/trait.Database.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["calimero_store",[]],["calimero_store_rocksdb",[["impl <a class=\"trait\" href=\"calimero_store/db/trait.Database.html\" title=\"trait calimero_store::db::Database\">Database</a>&lt;'_&gt; for <a class=\"struct\" href=\"calimero_store_rocksdb/struct.RocksDB.html\" title=\"struct calimero_store_rocksdb::RocksDB\">RocksDB</a>"]]]]);
+    var implementors = Object.fromEntries([["calimero_store",[]],["calimero_store_rocksdb",[["impl Database&lt;'_&gt; for <a class=\"struct\" href=\"calimero_store_rocksdb/struct.RocksDB.html\" title=\"struct calimero_store_rocksdb::RocksDB\">RocksDB</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[21,309]}
+//{"start":57,"fragment_lengths":[21,194]}
